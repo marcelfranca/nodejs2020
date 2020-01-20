afb8b65b583c373f64aff2bf72f4d5939b19c6a4
marcelfranca <Nan44aocw@#>
2020-01-19 21:42:12 -0300

    emoji com olhinhos olhando pra cima

--- project1/app.js
+++ project1/app.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- project1/routes/admin.js
+++ project1/routes/admin.js
@@ -2,18 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    console.log("In another the Middleware");
-    // res.send('<form action="/product" method="post"><input type="text" name="tittle">' +
-    //     '<button type="submit">Add Product</button></form>');
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+const products = [];
+
+router.get('/admin/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, nex) => {
-    console.log(req.body);
+router.post('/admin/add-product', (req, res, next) => {
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
+
