e8e519fad649014f36c4f33c83211c5ea4d804b5
marcelfranca <Nan44aocw@#>
2020-01-21 00:17:17 -0300

    rest_api 1 ended

--- rest_api/controlers/feed.js
+++ rest_api/controlers/feed.js
@@ -6,6 +6,7 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
 	const title = req.body.title;
 	const content = req.body.content;
+	console.log(title, content)
 	res.status(201).json({
 		message: 'Post created!',
 		post: { id: new Date().toISOString(), title: title, content: content }
