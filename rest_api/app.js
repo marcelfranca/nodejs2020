81cffe9d92ca8a5473d856a2607bf6ad1cd5ed4f
marcelfranca <Nan44aocw@#>
2020-01-25 18:41:37 -0300

    MongoDB added

--- rest_api/app.js
+++ rest_api/app.js
@@ -17,6 +17,6 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-mongoose.connect('MONGODB').then(result => {
+mongoose.connect('http//localhost:27017').then(result => {
     app.listen(8080);
 }).catch(err => console.log(err));
