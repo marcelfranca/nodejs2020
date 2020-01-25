87167ab921edcd478dccde045c3d658bd3ac7e9e
marcelfranca <Nan44aocw@#>
2020-01-25 20:37:19 -0300

    post editting

--- rest_api/controlers/feed.js
+++ rest_api/controlers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
@@ -67,3 +70,58 @@ exports.getPost = (req, res, next) => {
 		next(err);
 	})
 }
+
+exports.updatePost = (req, res, next) => {
+	const postId = req.params.postId;
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error ('Validation failed');
+		error.statusCode = 422;
+		throw error;
+	}
+	if (!req.file) {
+		const error = new Error('No image provided');
+		error.statusCode = 422;
+		throw error;
+	}
+	const title = req.body.title;
+	const content = req.body.content;
+	let imageUrl = req.body.imageUrl;
+	if (req.file) {
+		imageUrl = req.file.path;
+	}
+	if (!imageUrl) {
+		const error = new Error('No file uploaded');
+		error.statusCode = 422;
+		throw error;
+	}
+	Post.findById(postId)
+	.then(post => {
+		if (!post) {
+			const error = new Error('Post not found');
+			error.statusCode = 404;
+			throw error;
+		}
+		if (imageUrl !== post.imageUrl) {
+			clearImage(post.imageUrl);
+		}
+		post.title = title;
+		post.imageUrl = imageUrl;
+		post.content = content;
+		return post.save();
+	})
+	.then(result => {
+		res.status(200).json({ message: 'Post update', post: result });
+	})
+	.catch(err => {
+		if (!err.statusCode) {
+			err.statusCode = 500;
+		}
+		next(err);
+	});
+};
+
+const clearImage = filePath => {
+	filePath = path.join(__dirname, '..', filePath);
+	fs.unlink(filePath, err => console.log(err));
+};
